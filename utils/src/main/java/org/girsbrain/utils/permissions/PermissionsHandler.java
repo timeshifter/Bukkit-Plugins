8dfbe2c467a7920499d1c70da7edc6fb06391156
James Logsdon <dwarf@girsbrain.org>
2011-05-10 13:04:56 -0400

    [UTILS] Removing Group Manager support

--- utils/src/main/java/org/girsbrain/utils/permissions/PermissionsHandler.java
+++ utils/src/main/java/org/girsbrain/utils/permissions/PermissionsHandler.java
@@ -4,7 +4,6 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.Server;
 
 import com.nijikokun.bukkit.Permissions.Permissions;
-import org.anjocaido.groupmanager.GroupManager;
 
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -14,7 +13,6 @@ import org.bukkit.entity.Player;
  */
 public class PermissionsHandler {
     private enum HandlerType {
-        GROUP_MANAGER,
         PERMISSIONS,
         NONE
     };
@@ -27,13 +25,9 @@ public class PermissionsHandler {
             return;
         }
 
-        Plugin groupManager = server.getPluginManager().getPlugin("GroupManager");
         Plugin permissions = server.getPluginManager().getPlugin("Permissions");
 
-        if (groupManager != null) {
-            handler = HandlerType.GROUP_MANAGER;
-            plugin = groupManager;
-        } else if (permissions != null) {
+        if (permissions != null) {
             handler = HandlerType.PERMISSIONS;
             plugin = permissions;
         } else {
@@ -53,10 +47,6 @@ public class PermissionsHandler {
         switch (handler) {
             case PERMISSIONS:
                 return ((Permissions)plugin).getHandler().permission(player, permission);
-            case GROUP_MANAGER:
-                return ((GroupManager)plugin).getWorldsHolder().getWorldPermissions(player).has(player, permission);
-            case NONE:
-                return player.isOp();
             default:
                 return player.isOp();
         }
